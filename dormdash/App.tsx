78100f70bde1a1e7f35660a3be2a95a2baf869a1
arriellam <arriella@seas.upenn.edu>
2025-11-08 19:59:07 -0500

    Revert "Create profile screen navigation from feed"
    This reverts commit a087f82166a2fb9d6b7b49f2ead21b1cd74affe1.

--- dormdash/App.tsx
+++ dormdash/App.tsx
@@ -5,12 +5,6 @@ import Auth from "./components/Auth";
 import Feed from "./components/Feed";
 import { View } from "react-native";
 import type { Session } from "@supabase/supabase-js";
-import ProfileScreen from "./components/ProfileScreen";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [session, setSession] = useState<Session | null>(null);
@@ -25,16 +19,10 @@ export default function App() {
     });
   }, []);
 
-  if (!session) {
-    return <Auth />;
-  }
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Feed" component={Feed} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      {/* <Auth /> */}
+      {session ? <Feed /> : <Auth />}
+    </View>
   );
 }
--- dormdash/components/Feed.tsx
+++ dormdash/components/Feed.tsx
@@ -1,22 +1,23 @@
-import { useNavigation } from "@react-navigation/native";
-import { Button } from "@rneui/themed";
+import React from "react";
 import { View, Text } from "react-native";
+import { Button } from "@rneui/themed";
 import { supabase } from "../lib/supabase";
 
 const handleSignOut = async () => {
   const { error } = await supabase.auth.signOut();
-  if (error) console.error("Sign-out failed", error);
+  if (error) {
+    console.error("Sign-out failed", error);
+  }
 };
 
-export default function Feed() {
-  const navigation = useNavigation<any>();
-
+const Feed: React.FC = () => {
   return (
     <View>
       <Text>this is the feed</Text>
       <Button title="Post" />
-      <Button title="Profile" onPress={() => navigation.navigate("Profile")} />
       <Button title="Log out" onPress={handleSignOut} />
     </View>
   );
-}
+};
+
+export default Feed;
