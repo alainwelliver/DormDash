3053f637e96af968611f6253c13385c3d6d0f94b
arriellam <arriella@seas.upenn.edu>
2025-11-08 21:06:29 -0500

    Reapply "Create profile screen navigation from feed"
    This reverts commit 78100f70bde1a1e7f35660a3be2a95a2baf869a1.

--- dormdash/components/Feed.tsx
+++ dormdash/components/Feed.tsx
@@ -1,23 +1,22 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { Button } from "@rneui/themed";
+import { View, Text } from "react-native";
 import { supabase } from "../lib/supabase";
 
 const handleSignOut = async () => {
   const { error } = await supabase.auth.signOut();
-  if (error) {
-    console.error("Sign-out failed", error);
-  }
+  if (error) console.error("Sign-out failed", error);
 };
 
-const Feed: React.FC = () => {
+export default function Feed() {
+  const navigation = useNavigation<any>();
+
   return (
     <View>
       <Text>this is the feed</Text>
       <Button title="Post" />
+      <Button title="Profile" onPress={() => navigation.navigate("Profile")} />
       <Button title="Log out" onPress={handleSignOut} />
     </View>
   );
-};
-
-export default Feed;
+}
