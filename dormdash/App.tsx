19743cb5a783b9aea313a4ba46c2437f3b9cf74f
arriellam <arriella@seas.upenn.edu>
2025-11-13 19:28:21 -0500

    Update file structure to src and navigation folder No change to base functionality.

--- dormdash/App.tsx
+++ dormdash/App.tsx
@@ -1,28 +1,7 @@
 import "react-native-url-polyfill/auto";
-import { useState, useEffect } from "react";
-import { supabase } from "./lib/supabase";
-import Auth from "./components/Auth";
-import Feed from "./components/Feed";
-import { View } from "react-native";
-import type { Session } from "@supabase/supabase-js";
+import React from "react";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null);
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
-
-  return (
-    <View>
-      {/* <Auth /> */}
-      {session ? <Feed /> : <Auth />}
-    </View>
-  );
+  return <AppNavigator />;
 }
