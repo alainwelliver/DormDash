e9abd202bfc0580d2b1cf9333b85c049b9baabff
Xiang Chen <xianc@seas.upenn.edu>
2026-02-09 10:21:27 -0500

    deleted dormdash folder

--- dormdash/App.tsx
+++
@@ -1,7 +0,0 @@
-import "react-native-url-polyfill/auto";
-import React from "react";
-import AppNavigator from "./src/navigation/AppNavigator";
-
-export default function App() {
-  return <AppNavigator />;
-}
